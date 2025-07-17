3bee773305714aeeed6b57ed70c3b14afe292333
poetrainy <manamisato2001@gmail.com>
2025-07-17 14:23:12 +0900

    refactor: ts to js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const trigger = document.getElementById("trigger");
 const tooltip = document.getElementById("tooltip");
 
-const updateTooltip = (newState: boolean) => tooltip?.togglePopover(newState);
+const updateTooltip = (newState) => tooltip?.togglePopover(newState);
 
 trigger?.addEventListener("mouseover", () => updateTooltip(true));
 trigger?.addEventListener("mouseleave", () => updateTooltip(false));
